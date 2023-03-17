d0c8d61a5dd775487f7f6f2fcb27a805551d9bfb
peaceshi <peaceshi@outlook.com>
2023-03-17 22:16:55 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+using Windows.Win32;
+using Windows.Win32.System.Com;
+using Windows.Win32.UI.Shell;
+
+namespace CsWin32_Issue;
+
+public static class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        Directory.CreateDirectory("./out");
+
+        var path = Path.GetFullPath("./out");
+        //create a zip by yourself
+        var path1 = Path.GetFullPath("./test.zip");
+
+        PInvoke.CoCreateInstance(
+            typeof(Shell).GUID,
+            null,
+            CLSCTX.CLSCTX_INPROC_SERVER,
+            out IShellDispatch shell).ThrowOnFailure();
+        // Extract zip to dir.
+        shell.NameSpace(path).CopyHere(shell.NameSpace(path1).Items(), 16);
+    }
+}
